19a6b10452d4a4c95223cc3784dba9d0522737b4
snehasharmaax <147547884+snehasharmaax@users.noreply.github.com>
2023-10-26 10:53:11 +0000

    pending

---
+++ 01_basics/strings.js
@@ -0,0 +1,17 @@
+const name = "Sneha"
+const repoCount = 50
+
+// console.log(name + repoCount + "Value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('SnehaJii')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('h'));
+console.log(gameName.indexOf('h'));
