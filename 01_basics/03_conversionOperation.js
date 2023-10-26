f5354355890b623c65b4996e2eecc17291850270
snehasharmaax <147547884+snehasharmaax@users.noreply.github.com>
2023-10-26 09:23:42 +0000

    Intial File

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+let score = 33
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+// let score = "Sneha"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+// let isLoggedIn = 0
+// let isLoggedIn = ""
+// let isLoggedIn = "Sneha"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Sneha" =>true
+
+let someNumber = 33
+// let someNumber = "33"
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
