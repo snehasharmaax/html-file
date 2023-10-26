9b0a373d88587f7c256a2f5841ee87a6174005b1
snehasharmaax <147547884+snehasharmaax@users.noreply.github.com>
2023-10-26 15:46:21 +0000

    initial-file

--- 01_basics/strings.js
+++ 01_basics/strings.js
@@ -5,7 +5,7 @@ const repoCount = 50
 
 // console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('SnehaJii')
+const gameName = new String('Sneha-Jii')
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
@@ -14,4 +14,20 @@ const gameName = new String('SnehaJii')
 // console.log(gameName.toUpperCase());
 console.log(gameName.charAt(3));
 console.log(gameName.indexOf('h'));
-console.log(gameName.indexOf('h'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-6, 4)
+console.log(anotherString);
+
+const newStringOne = "   Sneha   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://sneha.com/sneha%20sharma"
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sneha'))
+
+console.log(gameName.split('-'));
