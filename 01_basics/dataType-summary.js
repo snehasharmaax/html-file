b5b335d5433cb15fd326fa1cd683fe131c369640
snehasharmaax <147547884+snehasharmaax@users.noreply.github.com>
2023-10-26 10:34:59 +0000

    modify

--- 01_basics/dataType-summary.js
+++ 01_basics/dataType-summary.js
@@ -28,3 +28,25 @@ const myfunc = function(){
     console.log("HEllo Sneha");
 }
 console.log(typeof myfunc);
+
+// ++++++++++++++++++++++++++++++++++ Stack & Heap Memory in Javascript+++++++++++++++++++++++++++++++++++++++++++
+// Stack (Primitive), Heap (Non-Primitive)
+
+
+let myName = "Sneha"
+let anotherName = myName
+anotherName = "Sanjana"
+
+console.log(myName);
+console.log(anotherName);
+
+let userOne = {
+    email : "userr@google.com",
+    upi : "user@ybl"
+}
+
+let userTwo = userOne
+userTwo.email = "Sneha@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
