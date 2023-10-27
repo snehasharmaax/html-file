04d92e1852f5f2bce82f076d5d2d26921ebbb011
snehasharmaax <147547884+snehasharmaax@users.noreply.github.com>
2023-10-27 14:46:09 +0000

    initial-file

---
+++ 02_basics/de-structuring.js
@@ -0,0 +1,17 @@
+const course = {
+    courseName: "JS in hindi",
+    price: "999",
+    courseInstructor : "sneha"
+}
+
+course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+const navbar = (company) => {
+
+}
+
--- 02_basics/objects2.js
+++ 02_basics/objects2.js
@@ -47,12 +47,15 @@ const users = [
 ]
 
 const user = users[1].email
-console.log(user);
+// console.log(user);
 
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+ 
