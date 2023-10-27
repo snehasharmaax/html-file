8d1ebbcde2e4d6367c838aafc270be13b7224920
snehasharmaax <147547884+snehasharmaax@users.noreply.github.com>
2023-10-27 14:32:07 +0000

    modified

--- 02_basics/objects2.js
+++ 02_basics/objects2.js
@@ -17,11 +17,42 @@ const regularUser = {
     }
 }
 
+// console.log(regularUser.fullName);
 // console.log(regularUser.fullName?.userFullName.firstName);
 
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "c", 4: "d"}
 
 // const obj3 = {obj1, obj2}
-const obj3 = Object.assign(obj1, obj2)
-console.log(obj3);
+// const obj3 = Object.assign(obj1, obj2)
+// const obj3 = Object.assign({}, obj1, obj2)
+const obj3 = {...obj1, ...obj2}
+
+// console.log(obj3);
+
+
+const users = [
+    {
+        id: 29374,
+        email: "Sneha@gmail.com"
+    },
+    {
+        id: 932384,
+        email: "sanjna2gmail.com"
+    },
+    {
+        id: 90384,
+        email: "lucky@gmail.com"
+    }
+]
+
+const user = users[1].email
+console.log(user);
+
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
