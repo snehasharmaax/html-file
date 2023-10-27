8a0ae758d6412d5cb455dddd790c7e1fd5ce9019
snehasharmaax <147547884+snehasharmaax@users.noreply.github.com>
2023-10-27 02:04:31 +0000

    initial-file

---
+++ 02_basics/array2.js
@@ -0,0 +1,26 @@
+const animate_hero = ["doraemon", "shinchan", "nobita", "chhota bheem", "tom & jerry"]
+const dc_heros = ["superman", "flash", "dalman"]
+// animate_hero.push(dc_heros)
+
+// console.log(animate_hero);
+// console.log(animate_hero[5][2]);
+
+// const allHeros = animate_hero.concat(dc_heros)
+// console.log(allHeros);
+
+// const all_new_heros = [...animate_hero, ...dc_heros]
+// console.log(all_new_heros);
+
+// const another_array = [1,2,3,[4,5,6], [7, 8, [9, 10, 15]]]
+// const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array);
+
+// console.log(Array.isArray("Sneha"));
+// console.log(Array.from("Sneha"));
+// console.log(Array.from({name: "Sneha"}));
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
