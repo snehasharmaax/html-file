e1e815ed6e8b02b6bff0c15e2c310509d380a437
snehasharmaax <147547884+snehasharmaax@users.noreply.github.com>
2023-10-29 08:04:00 +0000

    Initial-File

---
+++ 04_HigherOrder/filter.js
@@ -0,0 +1,36 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// const values = coding.forEach((item) => {
+//     // console.log(item);
+//     return item;
+// })
+// console.log(values);  //forEach does not return the value
+
+
+const myNums = [1,2,4,4,6,7,8,4,3,3]
+
+// const newNums = myNums.filter( (num) => {
+//     return num > 4
+// })
+// console.log(newNums);
+
+// const newNums = []
+// myNums.forEach( (num) => {
+//     if (num > 4) {
+//         newNums.push(num)
+//     }
+// })
+// console.log(newNums);
+
+const Books = [
+    { title: ' Book One', genre: 'Fiction', publish: 1981, edition: 2004},
+    { title: ' Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008},
+    { title: ' Book Three', genre: 'History', publish: 1999, edition: 2007},
+    { title: ' Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010},
+    { title: ' Book five', genre: 'Science', publish: 2009, edition: 2014},
+    { title: ' Book Six', genre: 'Fiction', publish: 1987, edition: 2010},
+    { title: ' Book Seven', genre: 'History', publish: 1986, edition: 1996},
+    { title: ' Book Eight', genre: 'Fiction', publish: 1989, edition: 2003},
+    { title: ' Book Nine', genre: 'Non-Fiction', publish: 1982, edition: 2002},
+    { title: ' Book Ten', genre: 'Science', publish: 1981, edition: 1996},
+]
